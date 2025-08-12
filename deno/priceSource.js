3a7e771ed608f0fa52bf8d0a72b5811d83353a8a
Arowana <arowana@mailum.com>
2025-08-12 17:44:02 +0000

    Move to optDeps (1.0.6)

--- deno/priceSource.js
+++ deno/priceSource.js
@@ -3,8 +3,9 @@
  * 
  * Because chainlink functions automation is time-based, we only check times not price changes
  */
-const ethers = await import("npm:ethers@6.15.0");
-const ethersOpt = await import("npm:ethers-opt@1.0.7");
+const ethers = await import('npm:ethers@6.15.0');
+const ethersOpt = await import('npm:ethers-opt@1.0.7');
+const arwGold = await import('npm:arowana-gold@1.0.5');
 
 const REMOTE_CHAIN = 42161;
 const REMOTE_CHAIN_RPC = 'https://arb1.arbitrum.io/rpc';
@@ -12,119 +13,10 @@ const REMOTE_CHAIN_ORACLE = '0x1F954Dc24a49708C26E0C1777f16750B5C6d5a2c';
 
 const HOME_CHAIN = 421614;
 const HOME_CHAIN_RPC = 'https://sepolia-rollup.arbitrum.io/rpc';
-const HOME_CHAIN_ORACLE = '0xB1Bcf13BAe2b914b4f59e0c835B9Ecf8b606c50c';
+const HOME_CHAIN_ORACLE = '0xBB7D041d5E2828569f4Bd667509AE15c3862298C';
 
 const ORACLE_DECIMALS = 8;
 const oracleInterval = 3600;
-
-const abi = [
-    {
-        "inputs":[
-            
-        ],
-        "name":"latestAnswer",
-        "outputs":[
-            {
-                "internalType":"int256",
-                "name":"",
-                "type":"int256"
-            }
-        ],
-        "stateMutability":"view",
-        "type":"function"
-    },
-    {
-        "inputs":[
-            
-        ],
-        "name":"latestRound",
-        "outputs":[
-            {
-                "internalType":"uint256",
-                "name":"",
-                "type":"uint256"
-            }
-        ],
-        "stateMutability":"view",
-        "type":"function"
-    },
-    {
-        "inputs":[
-            
-        ],
-        "name":"latestRoundData",
-        "outputs":[
-            {
-                "internalType":"uint80",
-                "name":"",
-                "type":"uint80"
-            },
-            {
-                "internalType":"int256",
-                "name":"",
-                "type":"int256"
-            },
-            {
-                "internalType":"uint256",
-                "name":"",
-                "type":"uint256"
-            },
-            {
-                "internalType":"uint256",
-                "name":"",
-                "type":"uint256"
-            },
-            {
-                "internalType":"uint80",
-                "name":"",
-                "type":"uint80"
-            }
-        ],
-        "stateMutability":"view",
-        "type":"function"
-    },
-    {
-        "inputs":[
-            
-        ],
-        "name":"latestTimestamp",
-        "outputs":[
-            {
-                "internalType":"uint256",
-                "name":"",
-                "type":"uint256"
-            }
-        ],
-        "stateMutability":"view",
-        "type":"function"
-    },
-    {
-      "inputs": [],
-      "name": "remoteChain",
-      "outputs": [
-        {
-          "internalType": "uint64",
-          "name": "",
-          "type": "uint64"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "remoteChainOracle",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-]
 
 // Chainlink Functions compatible Ethers JSON RPC provider class
 // (this is required for making Ethers RPC calls with Chainlink Functions)
@@ -136,8 +28,8 @@ class FunctionsJsonRpcProvider extends ethersOpt.Provider {
 
   async _send(payload) {
     const resp = await fetch(this.url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
     });
 
@@ -154,7 +46,7 @@ function getTimestamp() {
 async function fetchRemote() {
     try {
         const provider = new FunctionsJsonRpcProvider(REMOTE_CHAIN_RPC);
-        const dataFeed = new ethers.Contract(REMOTE_CHAIN_ORACLE, abi, provider);
+        const dataFeed = arwGold.contracts.DataFeed__factory.connect(REMOTE_CHAIN_ORACLE, provider);
         const currentTimestamp = getTimestamp();
         
         const [{ chainId }, [, answer, , updatedAt]] = await Promise.all([
@@ -186,7 +78,7 @@ async function fetchRemote() {
 async function fetchHome() {
     try {
         const provider = new FunctionsJsonRpcProvider(HOME_CHAIN_RPC);
-        const dataFeed = new ethers.Contract(HOME_CHAIN_ORACLE, abi, provider);
+        const dataFeed = arwGold.contracts.AGTPriceFeed__factory.connect(HOME_CHAIN_ORACLE, provider);
         const currentTimestamp = getTimestamp();
 
         const [{ chainId }, [, answer, , updatedAt], remoteChain, remoteChainOracle] = await Promise.all([
